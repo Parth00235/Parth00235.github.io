adb3b632ef14b05ffabd969d32daddcbe2e1c851
Parth00235 <parthsharma12391@gmail.com>
2024-07-09 09:53:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function fan() {
+   const b = document.getElementById("birthday").value
+   const c = document.getElementById("passion").value
+   alert("Hey! "+b+" Its your birthday today. You got one more year experience in "+c+  ".So, here is your cake 🎂🍰🍰🎂and your gift🎁💐.")
+   
+   var speaker = document.getElementById("mp1")
+   speaker.play()
+}
+
